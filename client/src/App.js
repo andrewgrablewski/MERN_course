1c80a0ab5015387f6e25643c27e9ec2fe5560943
andrew <agrablewski@gmail.com>
2020-07-26 11:33:59 -0400

    Fixed import issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import React, { Fragment, useEffect } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layouts/Navbar';
-import Landing from './components/layouts/Landing';
-import Login from './components/auth/Login';
-import Alert from './components/layouts/Alert';
-import Register from './components/auth/Register';
+import React, { Fragment, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Landing from "./components/layouts/Landing";
+import Login from "./components/auth/Login";
+import Alert from "./components/layouts/Alert";
+import Register from "./components/auth/Register";
 // Redux
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
-import { setAuthToken } from './utils/setAuthToken';
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -25,12 +25,12 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
+          <Route exact path="/" component={Landing} />
+          <section className="container">
             <Alert />
             <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
             </Switch>
           </section>
         </Fragment>
