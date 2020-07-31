119a9d2ebb49bf54a89c43f7491348cdf81d35f9
andrew <agrablewski@gmail.com>
2020-07-31 13:15:55 -0400

    Fix CreateProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Landing from './components/layouts/Landing';
 import Login from './components/auth/Login';
 import Alert from './components/layouts/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-form/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Register from './components/auth/Register';
 // Redux
@@ -35,6 +36,11 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
