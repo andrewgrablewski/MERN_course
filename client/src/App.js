67726f291f9db80a56f77edddffd0a72018039d1
andrew <agrablewski@gmail.com>
2020-08-02 20:38:43 -0400

    Add Experience and Education functionality

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-form/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import EditProfile from './components/profile-form/EditProfile';
+import AddExperience from './components/profile-form/AddExperience';
+import AddEducation from './components/profile-form/AddEducation';
 import Register from './components/auth/Register';
 // Redux
 import { Provider } from 'react-redux';
@@ -46,6 +48,16 @@ const App = () => {
                 exact
                 path='/edit-profile'
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
               />
             </Switch>
           </section>
