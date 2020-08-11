55fc7c68b8d8b01a9cdb7b154731a3c023a64701
andrew <agrablewski@gmail.com>
2020-08-11 11:06:55 -0400

    Add and remove posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import PostItem from './PostItem';
+import PostForm from './PostForm';
 import Spinner from '../layouts/Spinner';
 
 const Posts = ({ getPosts, post: { posts } }) => {
@@ -16,7 +17,7 @@ const Posts = ({ getPosts, post: { posts } }) => {
       <p className='lead'>
         <i className='fas fa-user'></i> Welcome to the community
       </p>
-
+      <PostForm />
       <div className='posts'>
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />
